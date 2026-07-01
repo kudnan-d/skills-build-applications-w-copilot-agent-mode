3f49a429f598a3fada756671ba86b0fe7f262727
Kundan D <kundandombale@outlook.com>
2026-07-01 12:27:31 +0000

    feat: update database connection paths and add new database connection file

--- octofit-tracker/backend/dist/scripts/seed.js
+++ octofit-tracker/backend/dist/scripts/seed.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.seedDatabase = seedDatabase;
-const database_1 = require("../database");
+const database_1 = require("../config/database");
 const User_1 = require("../models/User");
 const Team_1 = require("../models/Team");
 const Activity_1 = require("../models/Activity");
